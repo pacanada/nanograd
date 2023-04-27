b261cb95b0cd62426852ef463c0195eb3fbcc0c5
Pablo Canada Pereira <pacaa@dfds.com>
2023-04-27 16:06:32 +0200

    some dev

--- nanograd/exploration.py
+++ nanograd/exploration.py
@@ -7,6 +7,7 @@ class Value:
         self.data = data
         self._prev = _children
         self._op = _op
+        self.grad = None
 
     def __repr__(self) -> str:
         return f"Value({self.data})"
@@ -16,13 +17,20 @@ class Value:
     
     def __mul__(self, other: Value)->Value:
         return Value(self.data*other.data, (self, other), "mul")
+    
+    def backward(self):
+        if self.grad is None:
+            self.grad = 1
+        for value in self._prev:
+            value.backward()
 
 
 
 a = Value(1.0)
 b = Value(2.0)
+c = Value(3.0)
 
-c = a + b
+d = a + b + c
 
-graph = draw_dot(c)
+graph = draw_dot(d)
 graph.render()
--- nanograd/graph_utils.py
+++ nanograd/graph_utils.py
@@ -12,7 +12,7 @@ def trace(root):
     build(root)
     return nodes, edges
 
-def draw_dot(root, format='svg', rankdir='LR'):
+def draw_dot(root, format='png', rankdir='LR'):
     """
     format: png | svg | ...
     rankdir: TB (top to bottom graph) | LR (left to right)
@@ -22,7 +22,7 @@ def draw_dot(root, format='svg', rankdir='LR'):
     dot = Digraph(format=format, graph_attr={'rankdir': rankdir}) #, node_attr={'rankdir': 'TB'})
     
     for n in nodes:
-        dot.node(name=str(id(n)), label = "{ data %.4f | grad %.4f }" % (n.data, 0), shape='record')
+        dot.node(name=str(id(n)), label = "{ data %.4f | grad %.4f }" % (n.data, n.grad), shape='record')
         if n._op:
             dot.node(name=str(id(n)) + n._op, label=n._op)
             dot.edge(str(id(n)) + n._op, str(id(n)))
